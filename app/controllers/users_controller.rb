6cdddc87cf911b99c635ec27e69c4025f2d8c229
ketno.sakurai <s.k.sky123@gmail.com>
2020-11-21 05:02:49 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
+    @microposts = Micropost.all
   end
 
   def show
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @microposts = @user.likes.page(params[:page])
     counts(@user)
   end
     
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,9 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: {maximum: 255 }
+
+  has_many :favorites
+  #has_many :reverses_of_likes, class_name: 'Favorite', foreign_key: 'micropost_id'
+  #has_many :favors, through: :reverses_of_favorite, source: :user
+
 end
